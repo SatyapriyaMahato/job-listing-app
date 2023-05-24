7eb0cbff5b82e4145f8ebe16cbf4cfc6d5747e27
SatyapriyaMahato <satyapriyamahto@gmail.com>
2023-05-24 17:46:11 +0530

    add footer

--- src/App.js
+++ src/App.js
@@ -65,7 +65,10 @@ function App() {
           )
         })
       }
-
+      <div class="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
+        Coded by <a href="https://github.com/SatyapriyaMahato">Satyapriya Mahato</a>.
+      </div>
     </div>
   );
 }
